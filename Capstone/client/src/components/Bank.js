73c59af9531dd8ec58584ba1a263ad7a63ceff2c
Keaton Williamson <georgekwilliamson@gmail.com>
2020-02-13 13:15:42 -0600

    rough bank page is up

--- Capstone/client/src/components/Bank.js
+++ Capstone/client/src/components/Bank.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { createAuthHeaders } from '../API/userManager';
 import Design from './Design';
+import BankCard from './BankCard';
 import presetsManger from '../API/presetsManager';
 import { Loader, Dimmer, Button, Header, Icon, Modal } from 'semantic-ui-react'
 class Bank extends Component {
@@ -23,28 +24,32 @@ class Bank extends Component {
             });
     }
 
-    pushToPresetView(id) {
-        this.props.history.push(`/preset/${id}`)
+    pushToPresetView = (evt) => {
+        this.props.history.push(`/preset/${evt.target.id}`)
     }
 
-    deletePreset (id) {
-        presetsManger.deletePreset(id)
+    deletePreset = (evt) => {
+        presetsManger.deletePreset(evt.target.id)
         .then(() => this.renderBank())
     }
 
     render() {
         return (
             <>
+            <div style={{height: 'calc(100vh - 62px)', width: '100%', overflow: 'scroll', border: '7px solid red', display: 'flex', justifyContent: 'center' }}>
+            {/* <BankCard pushToPresetView={this.pushToPresetView} deletePreset={this.deletePreset}/> */}
                 {this.state.presets.map((preset, i) => (
-                    <div className="preset" key={i}>
-                        <p onClick={() => this.pushToPresetView(preset.id)} >{preset.presetName}</p>
-                        <p onClick={() => this.deletePreset(preset.id)}>DELETE</p>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                    </div>
+                    // <div className="preset" key={i}>
+                    //     <p onClick={() => this.pushToPresetView(preset.id)} >{preset.presetName}</p>
+                    //     <p onClick={() => this.deletePreset(preset.id)}>DELETE</p>
+                    //     <p></p>
+                    //     <p></p>
+                    //     <p></p>
+                    //     <p></p>
+                    // </div>
+                    <BankCard preset={preset} pushToPresetView={this.pushToPresetView} deletePreset={this.deletePreset} {...this.props}/>
                 ))}
+                </div>
             </>
         )
     }
